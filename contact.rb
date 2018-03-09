69bdb4644568a6ec088b11578238384bb41b736f
chronosxvi <arvin.peralta@gmail.com>
2018-03-09 15:18:26 +0800

    First version of the exercise file, contact.rb

---
+++ contact.rb
@@ -0,0 +1,6 @@
+first_name = "Arvin"
+last_name = "Peralta"
+email = "arvin.peralta@gmail.com"
+
+puts "Hello, it's me, #{first_name} #{last_name}."
+puts "Please contact me at my email address: #{email}"
